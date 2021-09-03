ce5fd8053c362f79ef83db6cbcf89dc8a32421ea
Welldy Rosman <welldyrosman@gmail.com>
2021-09-03 14:57:58 +0700

    action login

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -2,19 +2,49 @@
     <div class="container">
         <div class="center">
             <div class="col-7">
-                <input type="text" placeholder="ERP ID" class="form-control">
+                <input type="text" v-model="form.userid" placeholder="ERP ID" class="form-control">
                 <br>
-                <input type="password" placeholder="ERP PASSWORD" class="form-control">
+                <input type="password" v-model="form.password" placeholder="ERP PASSWORD" class="form-control">
+                <em><p class="notif tprimary">WRONG ID OR PASSWORD</p></em>
             </div>
             <div class="col-btn">
-                <div class="btn-login">
-                    <ion-icon class="icon-btn" name="checkmark-outline"></ion-icon>
+                <div v-on:click="login" class="btn-login" :class="btncolor">
+                    <ion-icon class="icon-btn" :name="btnname"></ion-icon>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            form:{
+                userid:"",
+                password:""
+            },
+            isloginoke:"",
+            btnname:"arrow-forward-outline",
+            btncolor:"primary"
+        }
+    },
+    methods:{
+        login(){
+            if(this.form.userid=="admin"&&this.form.userid=="123"){
+                this.btnname="checkmark-outline"
+                this.btncolor="success"
+            }else{
+                this.btnname="close-outline"
+                this.btncolor="danger"
+            }
+        }
+    }
+}
+</script>
 <style scoped>
+.notif{
+    text-align: right;
+}
 .container {
   display: flex;
   justify-content: center;
@@ -24,16 +54,31 @@
     margin-top:25%;
     font-size: 3rem;
 }
+.tprimary{
+    color: rgb(79, 170, 255);
+}
+.tdanger{
+    color: rgb(231, 58, 58);
+}
+.primary{
+    background: rgb(79, 170, 255);
+}
+.danger{
+    background: rgb(231, 58, 58);
+}
+.success{
+    background: rgb(38, 212, 204);
+}
 .btn-login{
     width: 100px;
     height: 100px;
     border-radius: 100%;
     border: none;
-    background: rgb(79, 170, 255);
     color: antiquewhite;
     margin-left: 1rem;
     margin-top: 20%;
     text-align: center;
+    cursor: pointer;
 }
 .center {
   width: 50%; 
