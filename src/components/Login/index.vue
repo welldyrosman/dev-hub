cc3d9f42647976469a0c84c87bcae8bbb59ea924
Welldy Rosman <welldyrosman@gmail.com>
2021-09-03 10:37:42 +0700

    add topmenu

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -1,11 +1,27 @@
 <template>
-<div>
-    <h1>Hallo</h1>
-    <ul>
-        <li>asdasda</li>
-         <li>asdasda</li>
-          <li>asdasda</li>
-    </ul>
-</div>
-    
+    <div class="container">
+        <div class="center">
+            <div class="col-8">
+                <input type="text" class="form-control">
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-4">
+                <button class="btn btn-primary">A</button>
+            </div>
+        </div>
+    </div>
 </template>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+.center {
+  width: 400px; 
+  padding: 10px;
+  background: #5F85DB;
+  color: #fff;
+  font-weight: bold;
+  font-family: Tahoma;
+}
+</style>
